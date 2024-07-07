af8327053c2dc3ffa13a1c3c63814f622a6c9da5
Ali Ahmad <ali@Alis-MacBook-Air.local>
2024-07-07 04:07:07 -0400

    Make changes based on new API since old one was discontinued

--- server.js
+++ server.js
@@ -1,63 +1,69 @@
-var fs = require("fs"),
-  exec = require("child_process").exec,
-  fetch = require("node-fetch");
+import fs from "fs";
+import { exec } from "child_process";
+import fetch from "node-fetch";
+import { createServer } from "http";
+
 var csldat, slcdat;
+
+exec("pip install -r requirements.txt");
+
 var updCDat = async () => {
   //every hour update countries.json
   //var data=await (await fetch("https://api.covid19api.com/countries")).json();
-  var data = (await (await fetch("https://api.covid19api.com/summary")).json())[
-    "Countries"
-  ];
+  var data = (
+    await (
+      await fetch(
+        "https://covid-api.com/api/regions?per_page=1002&order=name&sort=asc"
+      )
+    ).json()
+  )["data"];
   csldat = JSON.stringify(
-    Object.fromEntries(data.map(e => [e["Country"], e["Slug"]]))
+    Object.fromEntries(data.map((e) => [e["name"], e["iso"]]))
   );
   fs.writeFileSync("countriesSlug.json", csldat);
 };
 setInterval(updCDat, 3600000);
 updCDat().then(() => {
-  require("http")
-    .createServer((req, res) => {
-      var mimeType = "image/png";
-      if (req.url.endsWith(".css")) {
-        mimeType = "text/css";
-      } else if (["", "/"].includes(req.url) || req.url.endsWith(".html")) {
-        mimeType = "text/html";
-      }
-      res.writeHead(200, { "Content-Type": mimeType });
-      if (["", "/"].includes(req.url)) {
-        res.write(
-          fs
-            .readFileSync("public/index.html", "utf8")
-            .replace(
-              "***countries***",
-              fs.readFileSync("countriesSlug.json", "utf8")
-            )
-        );
-      } else if (req.url.startsWith("/c/")) {
-        if (!fs.existsSync("/tmp/c")) fs.mkdirSync("/tmp/c");
-        var execProcess = exec(
-          'python3 server.py "' +
-            req.url
-              .substr(3)
-              .toLowerCase()
-              .replace(/%20/g, " ")
-              .replace(/\"/g, "") +
-            '"'
-        );
-        execProcess.stdout.pipe(process.stdout);
-        execProcess.stderr.pipe(process.stderr);
-        execProcess.on("exit", function() {
-          var img =
-            "/tmp" + req.url.toLowerCase().replace(/%20/g, " ") + ".png";
-          if (fs.existsSync(img)) res.write(fs.readFileSync(img));
-          res.end();
-        });
-        return;
-      } else {
-        if (fs.existsSync("public" + req.url))
-          res.write(fs.readFileSync("public" + req.url, "utf8"));
-      }
-      res.end();
-    })
-    .listen(8080);
+  createServer((req, res) => {
+    var mimeType = "image/png";
+    if (req.url.endsWith(".css")) {
+      mimeType = "text/css";
+    } else if (["", "/"].includes(req.url) || req.url.endsWith(".html")) {
+      mimeType = "text/html";
+    }
+    res.writeHead(200, { "Content-Type": mimeType });
+    if (["", "/"].includes(req.url)) {
+      res.write(
+        fs
+          .readFileSync("public/index.html", "utf8")
+          .replace(
+            "***countries***",
+            fs.readFileSync("countriesSlug.json", "utf8")
+          )
+      );
+    } else if (req.url.startsWith("/c/")) {
+      if (!fs.existsSync("/tmp/c")) fs.mkdirSync("/tmp/c");
+      var execProcess = exec(
+        'python3 server.py "' +
+          req.url
+            .substr(3)
+            .toLowerCase()
+            .replace(/%20/g, " ")
+            .replace(/\"/g, "") +
+          '"'
+      );
+      execProcess.stdout.pipe(process.stdout);
+      execProcess.stderr.pipe(process.stderr);
+      execProcess.on("exit", function () {
+        var img = "/tmp" + req.url.toLowerCase().replace(/%20/g, " ") + ".png";
+        if (fs.existsSync(img)) res.write(fs.readFileSync(img));
+        res.end();
+      });
+      return;
+    } else {
+      if (fs.existsSync("public" + req.url))
+        res.write(fs.readFileSync("public" + req.url, "utf8"));
+    }
+    res.end();
+  }).listen(8080);
 });
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import os
 import sys
 from dateutil import parser
 from urllib.request import Request, urlopen
+from datetime import datetime, timedelta
 import time
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 9})
@@ -21,13 +22,16 @@ def lineValsGen(country, json_data):
     yvals = []
 
     for item in json_data:
-        date = item['Date'][5:10]
+        if not isinstance(item, dict):
+            continue
+
+        date = item['date'][5:10]
 
         if date in xvals:
             pass
         else:
             xvals.append(date)
-            yvals.append(item["Active"])
+            yvals.append(item["active"])
 
     xvals.reverse()
     yvals.reverse()
@@ -43,33 +47,33 @@ def graph(country, json_data):
     plt.title(title)
     plt.ylabel("Active Cases")
     plt.xlabel("Date")
-    plt.plot(xvals[-10:], yvals[-10:], label=slcjson[country])
+    plt.plot(xvals[-10:], yvals[-10:], label=slcjson[country.upper()])
     plt.legend()
     plt.savefig("/tmp/c/"+country+".png")
 
 
 def bar(country, json_data):
     labels = ["Total Cases", "Total Deaths", "Total Recoveries"]
-    totCases = json_data[0]["Confirmed"]
-    totDeaths = json_data[0]["Deaths"]
-    totRecov = json_data[0]["Recovered"]
+    totCases = json_data[0]["confirmed"]
+    totDeaths = json_data[0]["deaths"]
+    totRecov = json_data[0]["recovered"]
     values = [int(totCases) if totCases else 0, int(totDeaths)
               if totDeaths else 0, int(totRecov) if totRecov else 0]
     plt.bar(labels[0], values[0], label=labels[0])
     plt.bar(labels[1], values[1], label=labels[1])
     plt.bar(labels[2], values[2], label=labels[2])
-    plt.title("Statistics in " + slcjson[country])
+    plt.title("Statistics in " + slcjson[country.upper()])
     plt.legend()
     plt.savefig("/tmp/c/"+country+"__bar.png")
 
 
 def comp(country1, country2, json_data1, json_data2):
 
-    data1 = lineValsGen(slcjson[country1], json_data1)
+    data1 = lineValsGen(slcjson[country1.upper()], json_data1)
     xvals1 = data1[0]
     yvals1 = data1[1]
 
-    data2 = lineValsGen(slcjson[country2], json_data2)
+    data2 = lineValsGen(slcjson[country2.upper()], json_data2)
     xvals2 = data2[0]
     yvals2 = data2[1]
 
@@ -77,21 +81,28 @@ def comp(country1, country2, json_data1, json_data2):
     plt.title(title)
     plt.ylabel("Active Cases")
     plt.xlabel("Date")
-    plt.plot(xvals1[-10:], yvals1[-10:], label=slcjson[country1])
-    plt.plot(xvals2[-10:], yvals2[-10:], label=slcjson[country2])
+    plt.plot(xvals1[-10:], yvals1[-10:], label=slcjson[country1.upper()])
+    plt.plot(xvals2[-10:], yvals2[-10:], label=slcjson[country2.upper()])
     plt.legend()
     plt.savefig("/tmp/c/"+country1+"_"+country2+"__comp.png")
 
 
 def apiReq(countries):
+    today = datetime(2022, datetime.today().month, datetime.today().day)
+    # Get the last 7 dates including today
+    last_7_dates = [(today - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(7)]
     apiArr = []
     for item in countries:
-        response = requests.request(
-            "GET", "https://api.covid19api.com/total/country/"+item)
-        json_data = response.json()
-        json_data = sorted(json_data, key=lambda k: k['Date'])
-        json_data.reverse()
-        apiArr.append(json_data)
+        all_json_data = []
+        for date in last_7_dates:
+            response = requests.request(
+            "GET", "https://covid-api.com/api/reports/total?date=" + date + "&iso=" + item)
+            json_data = response.json()
+            all_json_data.append(json_data["data"])
+        if (all_json_data):
+            all_json_data = sorted(all_json_data, key=lambda k: k['date'])
+            all_json_data.reverse()
+        apiArr.append(all_json_data)
     return apiArr
 
 
